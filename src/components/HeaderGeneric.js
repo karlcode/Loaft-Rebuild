6824fb0746408afac33d329cb3bc4726e82aaab9
karl <karl.l1@hotmail.com>
2018-03-27 11:43:03 +1100

    Minor tweak on generic page

--- src/components/HeaderGeneric.js
+++ src/components/HeaderGeneric.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeaderGeneric = (props) => (
     <header id="header">
-        <h1>Generic</h1>
-        <p>Ipsum dolor sit amet nullam</p>
+        <h1>Generic Inner page</h1>
+        <p>Inner page</p>
     </header>
 )
 
